9f016ae9359317d36241a7c9cbc52a5af3b92068
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-03-26 21:38:56 +0000

    add authController

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function login(Request $request)
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid email or password'
+            ], 401);
+        }
+        $user = $request->user();
+        $token = $user->createToken('Access Token');
+        $user->access_token = $token->accessToken;
+        return response()->json([
+            'message' => 'Login successful',
+            ' user' => $user,
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function register(Request $request)
+    {
+        $defaultRole = Role::where('name', 'user')->first();
+
+        // Create the user with the default role attached
+        $user = $defaultRole->users()->create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json([
+            "message" => "User registered successfully"
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'message' => "User logged out successfully"
+        ], 200);
+    }
+
+    public function index()
+    {
+        echo "hello world";
+    }
+}
