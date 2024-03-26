1c4dda2bad24b5a6aab85468d11fb4e22f874c63
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-03-26 21:40:48 +0000

    add all routes nedded

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::apiResource('users', UserController::class);
+Route::namespace('Api')->group(function () {
+
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('register', [AuthController::class, 'register']);
+    });
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function () {
+        Route::get('helloworld', [AuthController::class, 'index']);
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
 });
