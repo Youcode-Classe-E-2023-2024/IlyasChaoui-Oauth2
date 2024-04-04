cdfe21ab32b5aad2fcd690f5bc0887a8d5259499
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-04-04 11:43:03 +0000

    add all api routes with middleware

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\PermissionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +21,16 @@ use App\Http\Controllers\Api\AuthController;
 // Route::apiResource('users', UserController::class);
 Route::namespace('Api')->group(function () {
 
+    /*
+    |--------------------------------------------------------------------------
+    | Forgot-password
+    |--------------------------------------------------------------------------
+    */
+    Route::post('/forgotPassword', [AuthController::class, 'forgotPassword']);
+    Route::get('/mot-de-passe/reinitialiser/{token}', [AuthController::class, 'showResetForm'])->name('password.reset');
+    Route::post('/mot-de-passe/reset/', [AuthController::class, 'reset']);
+
+
     Route::prefix('auth')->group(function () {
         Route::post('login', [AuthController::class, 'login']);
         Route::post('register', [AuthController::class, 'register']);
@@ -26,16 +38,42 @@ Route::namespace('Api')->group(function () {
     Route::group([
         'middleware' => 'auth:api'
     ], function () {
-        Route::get('helloworld', [AuthController::class, 'index']);
-        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('/helloworld', [AuthController::class, 'index']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+
+        Route::group([
+            'middleware' => 'checkRoles:api'
+        ], function () {
+
+            /*
+        |--------------------------------------------------------------------------
+        | CRUD permissions
+        |--------------------------------------------------------------------------
+        */
+            Route::get('/permissions', [PermissionController::class, 'showPermissions']);
+            Route::post('/permissions', [PermissionController::class, 'addPermission']);
+            Route::put('/permissions/{id}', [PermissionController::class, 'updatePermission']);
+            Route::delete('/permissions/{id}', [PermissionController::class, 'deletePermission']);
+
+            /*
+        |--------------------------------------------------------------------------
+        | CRUD roles
+        |--------------------------------------------------------------------------
+        */
+            Route::get('/roles', [RoleController::class, 'showRoles']);
+            Route::post('/roles', [roleController::class, 'addRole']);
+            Route::put('/roles/{id}', [RoleController::class, 'updateRole']);
+            Route::delete('/roles/{id}', [roleController::class, 'deleteRole']);
+
+            /*
+        |--------------------------------------------------------------------------
+        | CRUD users
+        |--------------------------------------------------------------------------
+        */
+            Route::get('/users', [UserController::class, 'showUsers']);
+            Route::post('/users', [UserController::class, 'addUser']);
+            Route::put('/users/{id}', [UserController::class, 'updateUser']);
+            Route::delete('/users/{id}', [UserController::class, 'deleteUser']);
+        });
     });
 });
-
-/*
-|--------------------------------------------------------------------------
-| CRUD roles
-|--------------------------------------------------------------------------
-*/
-Route::get('/roles', [RoleController::class, 'showRoles']);
-Route::post('/roles', [roleController::class, 'addRole']);
-Route::delete('/roles/{id}', [roleController::class, 'deleteRole']);
